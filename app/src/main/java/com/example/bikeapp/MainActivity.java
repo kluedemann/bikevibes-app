b1d1afe36e21eadfa59db8cb6b697971b46506e0
Kai Luedemann <kluedema@ualberta.ca>
2022-05-04 16:28:41 -0600

    Tracks Accelerometer

--- app/src/main/java/com/example/bikeapp/MainActivity.java
+++ app/src/main/java/com/example/bikeapp/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.bikeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private TextView[] dataViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        dataViews = new TextView[3];
+        dataViews[0] = findViewById(R.id.x_accel_text);
+        dataViews[1] = findViewById(R.id.y_accel_text);
+        dataViews[2] = findViewById(R.id.z_accel_text);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        updateAccel(event.values);
+    }
+
+    private void updateAccel(float[] values) {
+        String myStr;
+        for (int i = 0; i < 3; i++) {
+            myStr = String.format("%.2f", values[i]);
+            dataViews[i].setText(myStr);
+            //System.out.println(myStr);
+        }
     }
 }
