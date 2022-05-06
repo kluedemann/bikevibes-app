967154ac51d3f61478a6993568f7005b8d0f40ce
Kai Luedemann <kluedema@ualberta.ca>
2022-05-06 11:45:06 -0600

    Added comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hello World!
+# Summer 2022 Bike App
+
+Created by Kai Luedemann. Supervised by Professor Mario Nascimento
+
+## Description:
+
+This repository contains the code for an Android application used to crowdsource data about the 
+quality of bicycle pathways. This app tracks accelerometer and location data while biking and 
+(eventually) sends it to a web server to be aggregated.
--- app/src/main/java/com/example/bikeapp/MainActivity.java
+++ app/src/main/java/com/example/bikeapp/MainActivity.java
@@ -17,6 +17,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
     private static final String TAG = "MainActivity";
     private SensorManager mSensorManager;
@@ -28,8 +30,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get Accelerometer Sensor
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Collect Text boxes to display data
         dataViews = new TextView[3];
         dataViews[0] = findViewById(R.id.x_accel_text);
         dataViews[1] = findViewById(R.id.y_accel_text);
@@ -37,16 +43,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         locationViews = new TextView[2];
         locationViews[0] = findViewById(R.id.latitude_data);
         locationViews[1] = findViewById(R.id.longitude_data);
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        // Check if permissions have been granted, request them if not
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: ASSUMES PERMISSIONS ARE ALWAYS ACCEPTED
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+
+        // Enable location tracking
+        final int MIN_DELAY = 5 * 1000;
+        final int MIN_DIST = 10;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_DELAY, MIN_DIST, this);
     }
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     protected void onPause() {
@@ -55,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        // Sensor Accuracy Changed
     }
 
     public void onSensorChanged(SensorEvent event) {
@@ -64,19 +78,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void onLocationChanged(Location loc) {
+        //Log.d(TAG, String.format("%f, %f, %d", loc.getLatitude(), loc.getLongitude(), loc.getTime());
         updateLocation(loc.getLatitude(), loc.getLongitude());
     }
 
     private void updateAccel(float[] values) {
+        // Update accelerometer text boxes
         for (int i = 0; i < 3; i++) {
-            dataViews[i].setText(String.format("%.2f", values[i]));
+            dataViews[i].setText(String.format(Locale.getDefault(), "%.2f", values[i]));
         }
     }
 
     private void updateLocation(double latitude, double longitude) {
-        locationViews[0].setText(String.format("%f", latitude));
-        locationViews[1].setText(String.format("%f", longitude));
+        // Update location text boxes
+        locationViews[0].setText(String.format(Locale.getDefault(),"%f", latitude));
+        locationViews[1].setText(String.format(Locale.getDefault(), "%f", longitude));
     }
-
-
 }
